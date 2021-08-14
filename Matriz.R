a27fa8a10b848a5890d7c87e49d4a3a394b90f4c
bobyDeveloper <zS21004509@estudiantes.uv.mx>
2021-08-14 12:30:20 -0500

    Math

---
+++ Matriz.R
@@ -0,0 +1,11 @@
+matriz <- matrix(1:9, nrow = 3)
+
+# Mostrar la matriz original
+print("Matriz original:")
+print(matriz)
+
+transpuesta <- t(matriz)
+
+print("Matriz transpuesta:")
+print(transpuesta)
+
